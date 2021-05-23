fda8f1b6e88c2717e10d7af1a98919a659f98491
jlamberts <jonathan.lamberts@gmail.com>
2021-05-22 22:57:20 -0400

    initial implementation

--- src/NBFormat.jl
+++ src/NBFormat.jl
@@ -1,5 +1,18 @@
 module NBFormat
 
-# Write your package code here.
+using IJulia
+using JuliaFormatter
 
+function format_current_cell()
+    current_cell = IJulia.In[IJulia.n]
+    formatted = JuliaFormatter.format_text(current_cell)
+    if formatted != current_cell
+        IJulia.load_string(formatted, true)
+    end
+end
+
+enable_autoformat() = IJulia.push_preexecute_hook(format_current_cell)
+disable_autoformat() = IJulia.pop_preexecute_hook(format_current_cell)
+
+export enable_autoformat, disable_autoformat
 end
