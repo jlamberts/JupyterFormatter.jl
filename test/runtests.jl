f506485b83862c10fa3c242b7ac8a256594eb950
jlamberts <jonathan.lamberts@gmail.com>
2021-05-22 22:57:32 -0400

    add basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using NBFormat
+using IJulia
 using Test
 
-@testset "NBFormat.jl" begin
-    # Write your tests here.
+@testset "UnitTests" begin
+    # test that we succesfully add and remove our formatting hook
+    starting_hooks = length(IJulia.preexecute_hooks)
+    enable_autoformat()
+    @test length(IJulia.preexecute_hooks) == starting_hooks + 1
+    disable_autoformat()
+    @test length(IJulia.preexecute_hooks) == starting_hooks
 end
